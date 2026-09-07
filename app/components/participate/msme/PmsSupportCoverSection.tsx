dd74bf9c9044ce139b11993ad3ff016a74a5867f
sompal <sompal7678@gmailcom>
2026-09-07 12:54:14 +0530

    pms suport terms text fix

--- app/components/participate/msme/PmsSupportCoverSection.tsx
+++ app/components/participate/msme/PmsSupportCoverSection.tsx
@@ -337,11 +337,11 @@ const PmsSupportCoverSection: FC = () => {
                 className="
                   mt-[12px]
                   max-w-[330px]
-                  text-[12px]
+                  text-[14px]
                   font-medium
                   leading-[1.55]
                   text-[#151515]
-                  sm:text-[13px]
+                  text-justify
                 "
               >
                 For eligible MSE participation in an approved exhibition,
@@ -1129,15 +1129,14 @@ const PmsSupportCoverSection: FC = () => {
               bg-white
               px-[12px]
               py-[6px]
-              lg:grid-cols-[290px_1fr]
+              lg:grid-cols-[390px_1fr]
             "
           >
             {/* Terms */}
             <p
               className="
                 pr-[15px]
-                text-[12px]
-                font-semibold
+                text-[14px]
                 leading-[1.5]
                 text-[#252525]
                 lg:border-r
